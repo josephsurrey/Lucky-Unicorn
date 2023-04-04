7819b841cf6b00e7b8554345547831cf468b022b
josephsurrey <josephsurrey2018@gmail.com>
2023-04-04 17:51:42 +1200

    removed unnecessary parameters from get_valid_input function

--- instructions.py
+++ instructions.py
@@ -11,7 +11,7 @@ from initial_payment import initial_payment
 def instructions():
     print("Welcome to Lucky Unicorn\n"
           "By playing this game you are supporting Doctors without Borders")
-    played_before = get_valid_input("Have you played this game before? (Y/N)", str, ["Y", "N", "NO", "YES"], True, 0, 0)
+    played_before = get_valid_input("Have you played this game before? (Y/N)", str, ["Y", "N", "NO", "YES"], True)
     if played_before == "Y" or played_before == "YES":
         initial_payment()
     else:
