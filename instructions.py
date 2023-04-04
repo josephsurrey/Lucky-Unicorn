2906344b16d5d001f752b457a4dfdcb3ae0db063
josephsurrey <josephsurrey2018@gmail.com>
2023-04-04 17:45:04 +1200

    changed instructions to run initial_payment instead of start_round

--- instructions.py
+++ instructions.py
@@ -5,7 +5,7 @@ Instructions function for Lucky Unicorn game
 # import information from other files
 from setup import *
 from get_valid_input import get_valid_input
-from start_round import start_round
+from initial_payment import initial_payment
 
 
 def instructions():
@@ -13,7 +13,7 @@ def instructions():
           "By playing this game you are supporting Doctors without Borders")
     played_before = get_valid_input("Have you played this game before? (Y/N)", str, ["Y", "N", "NO", "YES"], True, 0, 0)
     if played_before == "Y" or played_before == "YES":
-        start_round()
+        initial_payment()
     else:
         print(INSTRUCTIONS)
-        start_round()
+        initial_payment()
