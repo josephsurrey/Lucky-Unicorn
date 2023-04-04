78c7c6941c1b113856ab959badce44896dbd40ec
josephsurrey <josephsurrey2018@gmail.com>
2023-04-04 17:11:14 +1200

    setup constants

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Written by Joseph Surrey 4/04.2023
+Constants to be used in project
+"""
+
+# How much the user wins from each token
+TOKEN_VALUE = [["unicorn", 5.0], ["zebra", 0.5], ["horse", 0.5], ["donkey", 0]]
+# Chance of each token being generated
+TOKEN_WEIGHT = [["unicorn", 0.1], ["zebra", 0.3], ["horse", 0.3], ["donkey", 0.3]]
+# Maximum spend per game
+MAX_SPEND = 10
+# How much each round costs
+ROUND_PRICE = 1
+# Instructions
+INSTRUCTIONS = ["Instructions"]
